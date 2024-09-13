3b5d0eaf4c9cb7ce22ed78273879ddcc74a72511
Bart Thate <rssbotd@gmail.com>
2024-09-13 23:12:06 +0200

    update init

--- objt/__init__.py
+++ objt/__init__.py
@@ -1,40 +1,94 @@
 # This file is placed in the Public Domain.
 
 
-"objects broker"
+"threads"
+
+
+import io
+import queue
+import threading
+import time
+import traceback
+import types as rtypes
 
 
 rpr = object.__repr__
 
 
-class Broker:
+class Thread(threading.Thread):
 
-    "Fleet"
+    "Thread"
 
-    objs = []
+    def __init__(self, func, thrname, *args, daemon=True, **kwargs):
+        super().__init__(None, self.run, thrname, (), {}, daemon=daemon)
+        self._result   = None
+        self.name      = thrname or (func and named(func)) or named(self).split(".")[-1]
+        self.out       = None
+        self.queue     = queue.Queue()
+        self.sleep     = None
+        self.starttime = time.time()
+        if func:
+            self.queue.put_nowait((func, args))
 
-    @staticmethod
-    def all():
-        "return all objects."
-        return Broker.objs
+    def __iter__(self):
+        return self
 
-    @staticmethod
-    def get(orig):
-        "return object by matching repr."
-        res = None
-        for obj in Broker.objs:
-            if rpr(obj) == orig:
-                res = obj
-                break
-        return res
+    def __next__(self):
+        yield from dir(self)
 
-    @staticmethod
-    def register(obj):
-        "add bot."
-        Broker.objs.append(obj)
+    def size(self):
+        "return qsize"
+        return self.queue.qsize()
+
+    def join(self, timeout=1.0):
+        "join this thread."
+        super().join(timeout)
+        return self._result
+
+    def run(self):
+        "run this thread's payload."
+        func, args = self.queue.get()
+        try:
+            self._result = func(*args)
+        except Exception as ex:
+            time.sleep(1.0)
+            later(ex)
+
+
+def named(obj):
+    "return a full qualified name of an object/function/module."
+    if isinstance(obj, rtypes.ModuleType):
+        return obj.__name__
+    typ = type(obj)
+    if '__builtins__' in dir(typ):
+        return obj.__name__
+    if '__self__' in dir(obj):
+        return f'{obj.__self__.__class__.__name__}.{obj.__name__}'
+    if '__class__' in dir(obj) and '__name__' in dir(obj):
+        return f'{obj.__class__.__name__}.{obj.__name__}'
+    if '__class__' in dir(obj):
+        return f"{obj.__class__.__module__}.{obj.__class__.__name__}"
+    if '__name__' in dir(obj):
+        return f'{obj.__class__.__name__}.{obj.__name__}'
+    return None
+
+
+def launch(func, *args, **kwargs):
+    "launch a thread."
+    nme = kwargs.get("name", named(func))
+    thread = Thread(func, nme, *args, **kwargs)
+    thread.start()
+    return thread
+
+
+from .errors import Errors, errors, later
 
 
 def __dir__():
     return (
-        'Broker',
+        'Errors',
+        'Thread',
+        'errors',
+        'later',
+        'launch'
     )
